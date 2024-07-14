f5be49dabec386a495ee4f979f367ef885ed07aa
Juliusz Maretzky <kontakt@juliuszmaretzky.pl>
2024-07-14 17:34:37 +0200

    gardener can move freely on garden

--- Deptacz/_scripts/gardener.gd
+++ Deptacz/_scripts/gardener.gd
@@ -14,26 +14,35 @@ func _process(delta):
 	moveGardener(delta)
 	
 func moveGardener(delta):
-	var actualTilePosition = game_manager.lawn[actualTileCoords.x][actualTileCoords.y].position
+	#var actualTilePosition = game_manager.lawn[actualTileCoords.x][actualTileCoords.y].position
+	#if (actualTilePosition - position).length() < 3:
+		#direction = Vector2.ZERO
+		#position = actualTilePosition
+	direction = Vector2.ZERO
+	if Input.is_action_pressed("moveUp"):
+			#if actualTileCoords.y > 0:
+				#actualTileCoords.y -= 1
+		direction += Vector2(0, -1)
+	if Input.is_action_pressed("moveDown"):
+			#if actualTileCoords.y < game_manager.lawnHeight-1:
+				#actualTileCoords.y += 1
+		direction += Vector2(0, 1)
+	if Input.is_action_pressed("moveLeft"):
+			#if actualTileCoords.x > 0:
+				#actualTileCoords.x -= 1
+		direction += Vector2(-1, 0)
+	if Input.is_action_pressed("moveRight"):
+			#if actualTileCoords.x < game_manager.lawnWidth-1:
+				#actualTileCoords.x += 1
+		direction += Vector2(1, 0)
+			
+	#direction = direction.normalized()
 	translate(direction * speed * delta)
-	if (actualTilePosition - position).length() < 3:
-		direction = Vector2.ZERO
-		position = actualTilePosition
-		
-	if direction == Vector2.ZERO:
-		if Input.is_action_pressed("moveUp"):
-			if actualTileCoords.y > 0:
-				actualTileCoords.y -= 1
-				direction += Vector2(0, -1)
-		elif Input.is_action_pressed("moveDown"):
-			if actualTileCoords.y < game_manager.lawnHeight-1:
-				actualTileCoords.y += 1
-				direction += Vector2(0, 1)
-		elif Input.is_action_pressed("moveLeft"):
-			if actualTileCoords.x > 0:
-				actualTileCoords.x -= 1
-				direction += Vector2(-1, 0)
-		elif Input.is_action_pressed("moveRight"):
-			if actualTileCoords.x < game_manager.lawnWidth-1:
-				actualTileCoords.x += 1
-				direction += Vector2(1, 0)
+	if position.x > game_manager.lawn[game_manager.lawnWidth-1][0].position.x:
+		position.x = game_manager.lawn[game_manager.lawnWidth-1][0].position.x
+	if position.x < game_manager.lawn[0][0].position.x:
+		position.x = game_manager.lawn[0][0].position.x
+	if position.y > game_manager.lawn[0][game_manager.lawnHeight-1].position.y:
+		position.y = game_manager.lawn[0][game_manager.lawnHeight-1].position.y
+	if position.y < game_manager.lawn[0][0].position.y:
+		position.y = game_manager.lawn[0][0].position.y
